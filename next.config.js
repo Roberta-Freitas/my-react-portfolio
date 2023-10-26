c8695ecc9cf023e07fdf2919e0b1113affd85969
Roberta Freitas <robertaefreitas@gmail.com>
2023-10-26 13:14:08 +0200

    deleted the server.js file to fix the problem with the images not showing when deployed to Heroku

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
-// module.exports = {
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       test: /\.(mov|mp4)$/,
-//       use: [
-//         {
-//           loader: 'file-loader',
-//           options: {
-//             publicPath: '/_next',
-//             outputPath: 'static/images', // Change this output path as needed
-//           },
-//         },
-//       ],
-//     });
-
-//     return config;
-//   },
-// };
 module.exports = {
-  images: {
-    loader: 'imgix',
-    path: 'https://robertafreitas-webdev.com/',
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mov|mp4)$/,
